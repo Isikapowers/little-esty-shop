ba67b7a778cecee2c307b09fc124c39a9511cfb0
iPowers <isika.powers@gmail.com>
2021-09-27 11:09:03 -0600

    Create bulkdiscounts controller.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,7 +6,7 @@ class BulkDiscountsController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @bulk_discount = @merchant.bulk_discounts.find(params[:id])
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 
   def new
@@ -16,7 +16,7 @@ class BulkDiscountsController < ApplicationController
 
   def create
     merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = merchant.bulk_discounts.new(discount_params)
+    bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
     if bulk_discount.save
       redirect_to merchant_bulk_discounts_path(merchant)
       flash[:success] = "Discount has been successfully created!"
@@ -26,10 +26,35 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+    if bulk_discount.update(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(merchant, bulk_discount)
+      flash[:success] = "Discount has been successfully updated!"
+    else
+      redirect_to edit_merchant_bulk_discount_path(merchant, bulk_discount)
+      flash[:alert] = "Error: Please fill in all the fields"
+    end
+  end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.bulk_discounts.destroy(params[:id])
+
+    redirect_to merchant_bulk_discounts_path(merchant)
+    flash[:danger] = "Discount has been deleted"
+  end
+
 
   private
 
-  def discount_params
+  def bulk_discount_params
     params.require(:bulk_discount).permit(:name, :percentage, :quantity)
   end
 end
